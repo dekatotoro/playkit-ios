4eee8e8f542a12084f52b04aad2d48ad0e98a819
Noam Tamim <noam.tamim@kaltura.com>
2017-01-19 13:28:57 +0200

    destination name and id are mutually exclusive.

--- TestApp/test_on_iphone7_sim.sh
+++ TestApp/test_on_iphone7_sim.sh
@@ -4,8 +4,8 @@ cd $(dirname $0)
 
 DESTINATION="platform=iOS Simulator,OS=10.2,name=iPhone 7"
 if [ "$TRAVIS" == "true" ]; then
-    # Workaround for travis-ci/travis-ci#7031 -- remove this entire if-fi when they fix it.
-    DESTINATION="$DESTINATION,id=22FA2149-1241-469C-BF6D-462D3837DB72"
+    # Workaround for travis-ci/travis-ci#7031 -- remove this entire if block when they fix it.
+    DESTINATION="platform=iOS Simulator,OS=10.2,id=22FA2149-1241-469C-BF6D-462D3837DB72"
 fi
 
 xcodebuild -workspace TestApp.xcworkspace/ -scheme TestApp_Tests test -destination "$DESTINATION" | tee xcode.log | xcpretty --color --utf
